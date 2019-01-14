826c382643f2ee5636c678a23a18bd38213959bf
AnastasiaSh <NastyaShugovskay24@yandex.by>
2019-01-14 13:14:36 +0300

    14.01 Deploy the project with firebase

---
+++ public/precache-manifest.e6d278ac5adcd51a2a5f73195d6091d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b47c2693b9270ee8d42561f06e118f0",
+    "url": "/static/media/logo.5b47c269.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6260766c1a7445b1e90d",
+    "url": "/static/js/main.6260766c.chunk.js"
+  },
+  {
+    "revision": "0ab75147a19c7b15fc81",
+    "url": "/static/js/1.0ab75147.chunk.js"
+  },
+  {
+    "revision": "6260766c1a7445b1e90d",
+    "url": "/static/css/main.1d9018d9.chunk.css"
+  },
+  {
+    "revision": "5eb375192f06106f23774b35f0e47bb5",
+    "url": "/index.html"
+  }
+];
